a1986c223c10e04ba274e4a8ec513155132d0301
Alec Horwitz <horwitz_alec@wheatoncollege.edu>
2018-02-08 01:46:54 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -2,6 +2,7 @@
 def run_guessing_game
   input = ""
   while input != "exit"
+    puts "Guess a number between 1 and 6."
     input = gets.strip
     roll = 1 + rand(6)
     input.to_i == roll ? puts "You guessed the correct number!" : "The computer guessed " + roll.to_s + "."
