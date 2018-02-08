e32922123caf282db3f3e17c14fcfcb4b01d006e
Alec Horwitz <horwitz_alec@wheatoncollege.edu>
2018-02-08 01:40:36 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -2,4 +2,5 @@
 def run_guessing_game
   input = gets.strip
   roll = 1 + rand(2 ** (64 - 2) - 1)
+  input == roll ? puts "You guessed the correct number!" : "The computer guessed " + roll.to_s + "."
 end
